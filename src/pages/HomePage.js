d493bf14c4996b6b10a1cde39b5e80a780df1430
Dane Grant <danecando@gmail.com>
2020-11-25 20:55:14 -0500

    completed home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,18 +1,28 @@
-import 'styled-components/macro'
 import Flex from '@primer/components/lib/Flex'
+import Box from '@primer/components/lib/Box'
+import StyledOcticon from '@primer/components/lib/StyledOcticon'
+import Text from '@primer/components/lib/Text'
+import { SearchIcon } from '@primer/octicons-react'
+import SearchForm from '../components/SeachForm'
 
 function HomePage() {
   return (
-    <Flex
-      alignItems="center"
-      justifyContent="center"
-      css={`
-        width: 100%;
-        min-height: 100vh;
-      `}
-    >
-      <h1>Home Page</h1>
-    </Flex>
+    <Box py="120px">
+      <Box maxWidth="1012px" m="auto" p="40px">
+        <Box>
+          <StyledOcticon icon={SearchIcon} size={24} />
+          <Text fontSize="24px" fontWeight="500" ml="16px">
+            Search GitHub Users
+          </Text>
+        </Box>
+        <SearchForm />
+        <Box>
+          <Text fontWeight="bold">ProTip! </Text>
+          <Text>Search for</Text>
+          <Text fontWeight="500"> danecando</Text>
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
